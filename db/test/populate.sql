4a84733ce893634aa3bbda8a795c708cadc4af1c
Brian Hiatt <bchiatt>
2014-12-15 12:25:51 -0600

    add db scripts

---
+++ db/test/populate.sql
@@ -0,0 +1,1 @@
+insert into users (id,username,password,avatar,token) values (1,'bob','$2a$08$PYFL3xyotOVDI/aAoCMdIOUmzLQ6k6yn5b29Vwoq7Uzgj8ff7CsYi','a.png','tok');
--- test/unit/user.js
+++ test/unit/user.js
@@ -10,18 +10,10 @@ var expect     = require('chai').expect,
     lab        = exports.lab = Lab.script(),
     describe   = lab.describe,
     it         = lab.it,
-    before     = lab.before,
     beforeEach = lab.beforeEach,
     db         = h.getdb();
 
 describe('User', function(){
-  before(function(done){
-    done();
-    // dbConnect(db, function(){
-    //   done();
-    // });
-  });
-
   beforeEach(function(done){
     cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
       done();
@@ -42,6 +34,32 @@ describe('User', function(){
     it('should register a new user', function(done){
       User.register({username:'sam', password:'1234', avatar:'http://images.apple.com/global/elements/flags/16x16/usa_2x.png'}, function(err){
         expect(err).to.be.null;
+        done();
+      });
+    });
+    it('should NOT register a new user - duplicate', function(done){
+      User.register({username:'bob', password:'1234', avatar:'http://images.apple.com/global/elements/flags/16x16/usa_2x.png'}, function(err){
+        expect(err).to.be.ok;
+        done();
+      });
+    });
+  });
+  describe('.login', function(){
+    it('should login a user', function(done){
+      User.login({username:'bob', password:'1234'}, function(user){
+        expect(user.username).to.equal('bob');
+        done();
+      });
+    });
+    it('should NOT login a user - bad username', function(done){
+      User.login({username:'wrong', password:'1234'}, function(user){
+        expect(user).to.be.undefined;
+        done();
+      });
+    });
+    it('should NOT login a user - bad password', function(done){
+      User.login({username:'bob', password:'wrong'}, function(user){
+        expect(user).to.be.undefined;
         done();
       });
     });
