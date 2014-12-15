28056a82d9c3538293b15461c66f6f3e65d85ea3
Brian Hiatt <bchiatt>
2014-12-15 14:15:36 -0600

    add dev deps

--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,1 +1,7 @@
+insert into users (id,username,password,avatar,token) values ('a1','b','c','d');
+insert into users (id,username,password,avatar,token) values ('a2','b','c','d');
+insert into users (id,username,password,avatar,token) values ('a3','b','c','d');
+
+delete from users;
+
 insert into users (id,username,password,avatar,token) values (1,'bob','$2a$08$PYFL3xyotOVDI/aAoCMdIOUmzLQ6k6yn5b29Vwoq7Uzgj8ff7CsYi','a.png','tok');
